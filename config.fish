dc7a537497a2ef56144f01e584373681b3523085
jmbeach <jaredbeachdesign@gmail.com>
2024-03-15 15:47:08 -0400

    init

---
+++ config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Uncomment to update fish_variables
+#set -U fish_user_paths /opt/nvim-linux64/bin $fish_user_paths
+#set -U fish_user_paths $HOME/.local/bin $fish_user_paths
+
+# Uncomment to update fish_variables
+#set -U fish_function_path $HOME/.config/fish/functions/colors $fish_function_path
---
+++ functions/colors/list-colors.fish
@@ -0,0 +1,69 @@
+# Displaying colors
+function list-colors
+    set_color black
+    echo -n "Black "
+    set_color --background black
+    echo "                                  "
+    set_color --background normal
+    echo -n "Red "
+    set_color --background red
+    echo "                                          "
+    set_color --background normal
+    echo -n "Green "
+    set_color --background green
+    echo "                                        "
+    set_color --background normal
+    echo -n "Yellow "
+    set_color --background yellow
+    echo "                                       "
+    set_color --background normal
+    echo -n "Blue "
+    set_color --background blue
+    echo "                                         "
+    set_color --background normal
+    echo -n "Magenta "
+    set_color --background magenta
+    echo "                                      "
+    set_color --background normal
+    echo -n "Cyan "
+    set_color --background cyan
+    echo "                                         "
+    set_color --background normal
+    echo -n "White "
+    set_color --background white
+    echo "                                        "
+    set_color --background normal
+    echo -n "Gray "
+    set_color --background brblack
+    echo "                                         "
+    set_color --background normal
+    echo -n "Bright Red "
+    set_color --background brred
+    echo "                                   "
+    set_color --background normal
+    echo -n "Bright Green "
+    set_color --background brgreen
+    echo "                                 "
+    set_color --background normal
+    echo -n "Bright Yellow "
+    set_color --background bryellow
+    echo "                                "
+    set_color --background normal
+    echo -n "Bright Blue "
+    set_color --background brblue
+    echo "                                  "
+    set_color --background normal
+    echo -n "Bright Magenta "
+    set_color --background brmagenta
+    echo "                               "
+    set_color --background normal
+    echo -n "Bright Cyan "
+    set_color --background brcyan
+    echo "                                  "
+    set_color --background normal
+    echo -n "Bright White "
+    set_color --background brwhite
+    echo "                                 "
+    set_color normal
+    set_color --background normal
+end

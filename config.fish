3762b99166afe6f453e27c7a81b2b209f39fcd48
jared.beach <jared.beach@capitalone.com>
2024-03-19 16:43:45 -0400

    Adds colors to fish.config

--- config.fish
+++ config.fish
@@ -3,9 +3,50 @@ if status is-interactive
 end
 
 # Uncomment to update fish_variables
+
 #set -U fish_user_paths /opt/nvim-linux64/bin $fish_user_paths
 #set -U fish_user_paths $HOME/.local/bin $fish_user_paths
 
-# Uncomment to update fish_variables
 #set -U fish_function_path $HOME/.config/fish/functions/colors $fish_function_path
 #set -U fish_function_path $HOME/.config/fish/functions/aliases $fish_function_path
+
+# Colorscheme: ayu Dark
+# set -U fish_color_normal B3B1AD
+# set -U fish_color_command 39BAE6
+# set -U fish_color_quote C2D94C
+# set -U fish_color_redirection FFEE99
+# set -U fish_color_end F29668
+# set -U fish_color_error FF3333
+# set -U fish_color_param B3B1AD
+# set -U fish_color_comment 626A73
+# set -U fish_color_match F07178
+# set -U fish_color_selection --background=E6B450
+# set -U fish_color_search_match --background=E6B450
+# set -U fish_color_history_current --bold
+# set -U fish_color_operator E6B450
+# set -U fish_color_escape 95E6CB
+# set -U fish_color_cwd 59C2FF
+# set -U fish_color_cwd_root red
+# set -U fish_color_valid_path --underline
+# set -U fish_color_autosuggestion 4D5566
+# set -U fish_color_user blue
+# set -U fish_color_host normal
+# set -U fish_color_cancel --reverse
+# set -U fish_pager_color_prefix normal --bold --underline
+# set -U fish_pager_color_progress brwhite --background=cyan
+# set -U fish_pager_color_completion normal
+# set -U fish_pager_color_description B3A06D
+# set -U fish_pager_color_selected_background --background=E6B450
+# set -U fish_color_option
+# set -U fish_pager_color_secondary_prefix
+# set -U fish_pager_color_selected_completion
+# set -U fish_color_host_remote
+# set -U fish_pager_color_secondary_completion
+# set -U fish_pager_color_selected_prefix
+# set -U fish_pager_color_secondary_background
+# set -U fish_pager_color_background
+# set -U fish_pager_color_selected_description
+# set -U fish_pager_color_secondary_description
+# set -U fish_color_keyword
+
+# set -U fish_greeting
